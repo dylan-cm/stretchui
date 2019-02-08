fb6fc1892a995c2c671dd91818a9fba0e3047fed
Dylan Modell <dylan.modell.promo@gmail.com>
2019-02-08 15:30:03 -0800

    moved artboard to dedicated class

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'organisms/stretchy_box.dart';
+import 'pages/artboard.dart';
 
 class App extends StatefulWidget{
   @override
@@ -7,25 +7,10 @@ class App extends StatefulWidget{
 }
 
 class _AppState extends State<App> {
-  double extPadding, intPadding, width, height;
-
-  void initState() { 
-    extPadding = intPadding = 10.0;
-    width = height = 200.0;
-    super.initState();
-  }
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold( //TODO: move to artboard screen file
-        body: Center(
-          child: StretchyBox(
-            extPadding:extPadding, 
-            intPadding:intPadding,
-          ),
-        )
-      )
+      home: Artboard(),
     );
   }
 }
---
+++ lib/src/pages/artboard.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import '../organisms/stretchy_box.dart';
+
+class Artboard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: StretchyBox(),
+      )
+    );
+  }
+}
