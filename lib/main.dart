8178624464fbf5fe17c87c9c7a980bd702aaec1c
Dylan Modell <dylan.modell.promo@gmail.com>
2019-02-08 01:30:07 -0800

    moved stertchybox to its own class

--- lib/main.dart
+++ lib/main.dart
@@ -19,31 +19,53 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: Container(
-          alignment: AlignmentDirectional.center,
-          color: Theme.of(context).primaryColorLight,
-          padding: EdgeInsets.all(extPadding),
-          width: width+extPadding,
-          height: height+extPadding,
-            child: Container(
-            alignment: AlignmentDirectional.center,
-            color: Theme.of(context).primaryColor,
-            padding: EdgeInsets.all(intPadding),
+      home: Scaffold( //TODO: move to artboard screen file
+        body: Center(
+          child: StretchyBox(
+            extPadding:extPadding, 
+            intPadding:intPadding,
             width: width,
             height: height,
-            child: Container(
-              alignment: AlignmentDirectional.center,
-              color: Theme.of(context).primaryColorDark,
-              padding: EdgeInsets.all(intPadding),
-              width: width-intPadding,
-              height: height-intPadding,
-            )
           ),
-        ),
+        )
       )
     );
   }
 }
 
-// class StretchyBox(double extPadding, intPadding, width, height){}
+class StretchyBox extends StatelessWidget{
+  final double extPadding, intPadding, width, height;
+  //TODO: add in parent widget with abstract class to pass in gesture detector
+  //TODO: add in suppport for passing a child like an image, video, or button
+  StretchyBox({
+    this.extPadding:0, 
+    this.intPadding:0, 
+    this.width:100, 
+    this.height:100
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: AlignmentDirectional.center,
+      color: Theme.of(context).primaryColorLight,
+      padding: EdgeInsets.all(extPadding),
+      width: width+extPadding,
+      height: height+extPadding,
+        child: Container(
+        alignment: AlignmentDirectional.center,
+        color: Theme.of(context).primaryColor,
+        padding: EdgeInsets.all(intPadding),
+        width: width,
+        height: height,
+        child: Container(
+          alignment: AlignmentDirectional.center,
+          color: Theme.of(context).primaryColorDark,
+          padding: EdgeInsets.all(intPadding),
+          width: width-intPadding,
+          height: height-intPadding,
+        )
+      ),
+    );
+  }
+}
