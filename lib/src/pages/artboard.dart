4c94e1e2734d5da8ef3b0c79a21a5490063f341c
Dylan Modell <dylan.modell.promo@gmail.com>
2019-02-08 22:01:54 -0800

    artboard hooked up to bloc, FAB to add stretchyboxes

--- lib/src/pages/artboard.dart
+++ lib/src/pages/artboard.dart
@@ -1,12 +1,28 @@
 import 'package:flutter/material.dart';
 import '../organisms/stretchy_box.dart';
+import 'artboard_bloc.dart';
+import '../bloc_provider.dart';
 
 class Artboard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: StretchyBox(),
+    final bloc = ArtBloc();
+    return BlocProvider(
+      bloc: bloc,
+      child: Scaffold(
+        body: Center(
+          child: StreamBuilder(
+            stream: bloc.stretchyStream,
+            initialData: Stack(),
+            builder: (context, snapshot){
+              return bloc.stretchyStack;
+            },
+          )
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add_box),
+          onPressed: bloc.addStretchy,
+        ),
       )
     );
   }
