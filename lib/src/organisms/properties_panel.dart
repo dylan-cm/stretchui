3818797ce3a619ca9783bf02bc9e901ca852eec1
Dylan Modell <dylan.modell.promo@gmail.com>
2019-02-15 02:26:55 -0800

    BAD BRANCH: significant refactor needed 2 increase scope of s.box models

--- lib/src/organisms/properties_panel.dart
+++ lib/src/organisms/properties_panel.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
+import '../bloc_provider.dart';
+import 'stretchy_bloc.dart';
 
 class PropPanel extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final Size screen = MediaQuery.of(context).size;
+    final StretchyBloc bloc = BlocProvider.of<StretchyBloc>(context);
+    //BAD!!!!!! No access to correct properties within bloc in tree
+    //Artboard --- ArtBloc --- StetchyBoxes --- StretchyBlocs
+    //                     `-- PropertyPanels
+    //Significant refactor needed before proceeding
     return Container(
       alignment: AlignmentDirectional.center,
       width: screen.width*0.9,
@@ -14,23 +21,22 @@ class PropPanel extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: <Widget>[
           Expanded(flex: 8, child: Container()),
-          const Text('X '),
-          Container(
-            width: 44,
-            height: 44,
-            child: const TextField(),
-          ),
-          const Text('Y '),
-          Container(
-            width: 44,
-            height: 44,
-            child: const TextField(
-              keyboardType: TextInputType.number,
-            ),
-          ),
+          _buildDisplay(bloc, 'X'),
+          _buildDisplay(bloc, 'Y'),
           Expanded(flex: 8, child: Container()),
         ],
       ),
+    );
+  }
+
+  //Builder method for displaying x & y
+  Widget _buildDisplay(StretchyBloc bloc, String axis){
+    return StreamBuilder<Param>(
+      stream: bloc.paramStream,
+      builder: (BuildContext context, AsyncSnapshot<Param> snapshot){
+        final int value = ( (axis == 'X') ? snapshot.data.x*100 : snapshot.data.y*100 ).floor();
+        return Text('$axis: $value');
+      },
     );
   }
 }
